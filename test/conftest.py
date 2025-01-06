6d4e4fecffeb021ddcad4ae51fd5e60c5ab2754f
Aleksei <ean1@bk.ru>
2025-01-06 19:23:32 +0300

    Добавление проверки текущего состояния сессии

--- test/conftest.py
+++ test/conftest.py
@@ -7,19 +7,19 @@ fixture = None
 @pytest.fixture(scope='session')
 def app(request):
     global fixture
-    fixture = Application()
-    fixture.session.login(username="admin", password="secret")
-    if not fixture.is_valid():
+    if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
-
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+    fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
 
 @pytest.fixture(scope='session', autouse=True)
 def stop(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
 
     request.addfinalizer(fin)
